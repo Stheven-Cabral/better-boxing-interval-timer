7e4bee41a01caa45bb51ecb4bd735603768803c4
SCabCodes <stheven.cabral@gmail.com>
2025-03-31 09:32:35 -0700

    033125 - Add logic to display error when settings inputs are invalid

--- components/Settings/index.tsx
+++ components/Settings/index.tsx
@@ -16,6 +16,50 @@ export const Settings = ({
     }
   };
 
+  const validateRoundsInput = (roundsInput: HTMLInputElement) => {
+    const maxValue = 100;
+    const inputValue = parseInt(roundsInput.value, 10);
+    const errorElement = document.getElementById("rounds-error");
+
+    if (isNaN(inputValue) || inputValue < 1 || inputValue > maxValue) {
+      errorElement!.textContent = `Invalid rounds input value: ${roundsInput.value}. Please enter a value between 1 and ${maxValue}.`;
+      return false;
+    } else {
+      errorElement!.textContent = "";
+    }
+
+    return true;
+  };
+
+  const validateTimeInput = (
+    timeInput: HTMLInputElement,
+    type: "minutes" | "seconds",
+    errorElementId: string
+  ) => {
+    const secondsMaxValue = 59;
+    const inputValue = parseInt(timeInput.value, 10);
+    const errorElement = document.getElementById(errorElementId);
+    console.log("validateTimeInput");
+
+    if (type === "minutes") {
+      if (isNaN(inputValue)) {
+        errorElement!.textContent = `Invalid minutes input value: ${timeInput.value}. Please enter a valid number.`;
+        return false;
+      } else {
+        errorElement!.textContent = "";
+      }
+    } else if (type === "seconds") {
+      if (isNaN(inputValue) || inputValue < 0 || inputValue > secondsMaxValue) {
+        errorElement!.textContent = `Invalid seconds input value: ${timeInput.value}. Please enter a value between 0 and ${secondsMaxValue}.`;
+        return false;
+      } else {
+        errorElement!.textContent = "";
+      }
+    }
+
+    return true;
+  };
+
   const setSettingsValues = () => {
     const roundsInput = document.getElementById("rounds") as HTMLInputElement;
     const roundMinutesInput = document.getElementById(
@@ -25,17 +69,37 @@ export const Settings = ({
       "time-per-round-seconds"
     ) as HTMLInputElement;
     const breakMinutesInput = document.getElementById(
-      "time-per-round-seconds"
+      "time-per-round-minutes"
     ) as HTMLInputElement;
     const breakSecondsInput = document.getElementById(
       "time-per-round-seconds"
     ) as HTMLInputElement;
 
-    console.log(roundsInput.value);
-    console.log(roundMinutesInput.value);
-    console.log(roundSecondsInput.value);
-    console.log(breakMinutesInput.value);
-    console.log(breakSecondsInput.value);
+    if (
+      !validateRoundsInput(roundsInput) ||
+      !validateTimeInput(
+        roundMinutesInput,
+        "minutes",
+        "minutes-per-round-error"
+      ) ||
+      !validateTimeInput(
+        roundSecondsInput,
+        "seconds",
+        "seconds-per-round-error"
+      ) ||
+      !validateTimeInput(
+        breakMinutesInput,
+        "minutes",
+        "minutes-per-break-error"
+      ) ||
+      !validateTimeInput(
+        breakSecondsInput,
+        "seconds",
+        "seconds-per-break-error"
+      )
+    ) {
+      return;
+    }
   };
 
   useEffect(() => {
@@ -60,6 +124,7 @@ export const Settings = ({
             Rounds:
           </label>
           <div className="rounds-input-group">
+            <p id="rounds-error" style={{ color: "red" }}></p>
             <input
               className="settings-input rounds-input"
               type="number"
@@ -71,11 +136,13 @@ export const Settings = ({
         <div className="settings-input-container">
           <label
             className="settings-input-label"
-            htmlFor="time-per-round-minutes"
+            htmlFor="time-per-round-minutes time-per-round-seconds"
           >
             Time per Round (MM:SS):
           </label>
           <div className="time-input-group">
+            <p id="minutes-per-round-error" style={{ color: "red" }} />
+            <p id="seconds-per-round-error" style={{ color: "red" }} />
             <input
               className="settings-input"
               type="number"
@@ -97,14 +164,19 @@ export const Settings = ({
         </div>
 
         <div className="settings-input-container">
-          <label className="settings-input-label" htmlFor="break-time">
+          <label
+            className="settings-input-label"
+            htmlFor="time-per-break-minutes time-per-break-seconds"
+          >
             Break Time (MM:SS):
           </label>
           <div className="time-input-group">
+            <p id="minutes-per-break-error" style={{ color: "red" }} />
+            <p id="seconds-per-break-error" style={{ color: "red" }} />
             <input
               className="settings-input"
               type="number"
-              id="break-time-minutes"
+              id="time-per-break-minutes"
               placeholder="00"
               min="0"
               max="59"
@@ -113,7 +185,7 @@ export const Settings = ({
             <input
               className="settings-input"
               type="number"
-              id="break-time-seconds"
+              id="time-per-break-seconds"
               placeholder="00"
               min="0"
               max="59"
